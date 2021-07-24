edae3223af2cd71653d78f53e679f17b7f6ae08f
HyunHan-Ji <83688992+HyunHan-Ji@users.noreply.github.com>
2021-07-24 20:55:29 +0900

    Add files via upload

---
+++ code/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace DongGyu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBox_open = new System.Windows.Forms.TextBox();
+            this.txtBox_save = new System.Windows.Forms.TextBox();
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_run = new System.Windows.Forms.Button();
+            this.txtBox_filename = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "변환파일";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "저장위치";
+            // 
+            // txtBox_open
+            // 
+            this.txtBox_open.Location = new System.Drawing.Point(93, 32);
+            this.txtBox_open.Name = "txtBox_open";
+            this.txtBox_open.Size = new System.Drawing.Size(365, 21);
+            this.txtBox_open.TabIndex = 2;
+            // 
+            // txtBox_save
+            // 
+            this.txtBox_save.Location = new System.Drawing.Point(93, 77);
+            this.txtBox_save.Name = "txtBox_save";
+            this.txtBox_save.Size = new System.Drawing.Size(365, 21);
+            this.txtBox_save.TabIndex = 3;
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(464, 30);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.TabIndex = 4;
+            this.btn_open.Text = "찾아보기";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(464, 75);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 5;
+            this.btn_save.Text = "찾아보기";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(165, 161);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(211, 69);
+            this.btn_run.TabIndex = 6;
+            this.btn_run.Text = "실행";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.btn_run_Click);
+            // 
+            // txtBox_filename
+            // 
+            this.txtBox_filename.Location = new System.Drawing.Point(93, 117);
+            this.txtBox_filename.Name = "txtBox_filename";
+            this.txtBox_filename.Size = new System.Drawing.Size(365, 21);
+            this.txtBox_filename.TabIndex = 9;
+            this.txtBox_filename.Text = "Result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "파일명";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 20F);
+            this.label4.Location = new System.Drawing.Point(31, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 27);
+            this.label4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 321);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBox_filename);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_run);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_open);
+            this.Controls.Add(this.txtBox_save);
+            this.Controls.Add(this.txtBox_open);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "JSON_To_Excel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBox_open;
+        private System.Windows.Forms.TextBox txtBox_save;
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.TextBox txtBox_filename;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ code/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.CodeDom.Compiler;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace DongGyu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            txtBox_open.Text = openFileDialog.FileName;
+        }
+
+        FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog.ShowDialog();
+            txtBox_save.Text = folderBrowserDialog.SelectedPath;
+        }
+
+        DataTable dt = new DataTable();
+        private void btn_run_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                label4.Text = "시작";
+
+                String data_str = File.ReadAllText(@txtBox_open.Text);
+                Console.Write(data_str);
+                JObject data_json = JObject.Parse(data_str);
+
+
+                label4.Text = "Json 변환중";
+                for (int k = 0; k < data_json["data"].Count(); k++)
+                {
+                    for (int i = 0; i < data_json["data"][k].Count(); i++)
+                    {
+                        string[] words = data_json["data"][k][i].ToString().Substring(1, data_json["data"][k][i].ToString().Length - 4).Split(',');
+                        for (int j = 0; j < 7; j++)
+                        {
+                            words[j] = words[j].Substring(4);
+                        }
+                        add_row(words);
+                    }
+                }
+
+                SaveToExcel();
+
+                label4.Text = "실행 완료";
+            }
+            catch (Exception ex)
+            {
+                label4.Text = ex.Message;//"오류발생";
+            }
+        }
+        private void add_row(string[] words)
+        {
+            DataRow row = dt.NewRow();
+            row["A"] = Convert.ToDouble(words[0]);
+            row["B"] = Convert.ToDouble(words[1]);
+            row["C"] = Convert.ToDouble(words[2]);
+            row["D"] = Convert.ToDouble(words[3]);
+            row["E"] = Convert.ToDouble(words[4]);
+            row["F"] = Convert.ToDouble(words[5]);
+            row["G"] = Convert.ToDouble(words[6]);
+            //row["H"] = Convert.ToDouble(words[7]);
+            // row["I"] = Convert.ToDouble(words[8]);
+            dt.Rows.Add(row);
+        }
+
+        private void SaveToExcel()
+        {
+            label4.Text = "저장중";
+
+            string path = @txtBox_save.Text + "\\" + txtBox_filename.Text + ".xlsx";
+            var excelApp = new Excel.Application();
+            excelApp.Workbooks.Add();
+
+            Excel._Worksheet workSheet = (Excel._Worksheet)excelApp.ActiveSheet;
+
+            for (var i = 0; i < dt.Rows.Count; i++)
+            {
+                for (var j = 0; j < dt.Columns.Count; j++)
+                {
+                    workSheet.Cells[i + 1, j + 1] = dt.Rows[i][j];
+                }
+            }
+
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            workSheet.SaveAs(path);
+            excelApp.Quit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add(new DataColumn("A", typeof(double)));
+            dt.Columns.Add(new DataColumn("B", typeof(double)));
+            dt.Columns.Add(new DataColumn("C", typeof(double)));
+            dt.Columns.Add(new DataColumn("D", typeof(double)));
+            dt.Columns.Add(new DataColumn("E", typeof(double)));
+            dt.Columns.Add(new DataColumn("F", typeof(double)));
+            dt.Columns.Add(new DataColumn("G", typeof(double)));
+            //dt.Columns.Add(new DataColumn("H", typeof(double)));
+            // dt.Columns.Add(new DataColumn("I", typeof(double)));
+        }
+    }
+}
